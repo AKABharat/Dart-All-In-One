216ce91177216e087fa13d1ac82424f20ec4dafd
Bharat <107737002+AKABharat@users.noreply.github.com>
2023-01-11 18:12:19 +0530

    Create main-revision.dart

---
+++ main-revision.dart
@@ -0,0 +1,64 @@
+// TO USE THE CODE , PLEASE UNCOMMENT THE CODE 
+// program written using dartpad
+void main(){
+//   print('hello world');
+
+//**************************************
+  
+// TOPIC : datatypes in dart
+// int bool(true/false) double float String
+
+// var name = 'bharat doodi'; // String
+// var age = 20; // int
+// var student = true; // bool
+// var marks = 50.00; // float
+
+// print(name);
+// print(age);
+// print(student);
+// print(marks);
+ 
+//**************************************  
+  
+ // TOPIC : string concatination and string interpolation
+  
+//   var str1 = 'hello';
+//   var str2 = 'world';
+//   // string concatination 
+//   print(str1+" "+str2);
+//   // string interpolation
+//   print('$str1 $str2'); // or print('${str1} ${str2}');
+  
+  
+  // we can even evalute a expression in string interpolation
+//   int num = 10;
+//   int('${num*10+20}'); // 120 output
+  
+ //**************************************
+  
+// TOPIC : string escape "\" and string raw "r"
+  // if you want to use ' or " inside a string then we use string escape \ 
+  // if you want to use \ then we use string raw r
+  
+ // string escape to use ' or " 
+//   print('today i\'m feeling good');
+// string escape to use \
+//   print(r'c:\window\desktop');
+  
+//**************************************
+  
+//TOPIC : multiline string 
+//   String s = """
+//   here we are printing multiline string
+//   """;
+//   print(s);
+  // NOTE : here string will be printed in the order you fit string in it 
+//   string dis = """
+//String another ="""
+//hello     there 
+//      how  are           you ???   ?
+//""";
+//  print(another);
+ 
+//**************************************
+}
